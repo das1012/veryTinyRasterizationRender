178192c1a8bf98cbbf5fae6245fc34096792f78f
Mingrui Liu <mingruiliu94@gmail.com>
2020-04-17 01:14:29 +0800

    add line & trinangle

---
+++ Render/Render.cpp
@@ -0,0 +1,126 @@
+﻿// Render.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#define NOMINMAX
+
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <memory>
+#include "tgaimage.h"
+#include "model.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+	bool steep = false;
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	if (x0 > x1) {
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int derror2 = std::abs(dy) * 2;
+	int error2 = 0;
+	int y = y0;
+	for (int x = x0; x <= x1; x++) {
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+		error2 += derror2;
+		if (error2 > dx) {
+			y += (y1 > y0 ? 1 : -1);
+			error2 -= dx * 2;
+		}
+	}
+}
+
+Vec3f barycentric(std::vector<Vec2i> pts, Vec2i P) {
+	Vec3f u = cross(Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]), Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]));
+	/* `pts` and `P` has integer value as coordinates
+	   so `abs(u[2])` < 1 means `u[2]` is 0, that means
+	   triangle is degenerate, in this case return something with negative coordinates */
+	if (std::abs(u[2]) < 1) return Vec3f(-1, 1, 1);
+	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(std::vector<Vec2i> pts, TGAImage& image, TGAColor color) {
+	// 3x2 pts 是三角形的三个顶点
+	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bboxmax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			bboxmin[j] = std::max(0, std::min(bboxmin[j], pts[i][j]));
+			bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
+		}
+	}
+	Vec2i P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
+			Vec3f bc_screen = barycentric(pts, P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
+			image.set(P.x, P.y, color);
+		}
+	}
+}
+
+Vec3f cross_multiply(Vec3f a, Vec3f b) {
+	return Vec3f(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+}
+
+
+
+const int width = 500;
+const int height = 500;
+
+int main(int argc, char** argv) {
+	TGAImage image(500, 500, TGAImage::RGB);
+
+	
+	using namespace std::chrono;
+	Vec3f light_dir(0, 0, -1);
+	std::shared_ptr<Model> model = std::make_shared<Model>("C:/Users/LiuMingrui/source/repos/Render/Render/obj/african_head/african_head.obj");
+	for (int i = 0; i < model->nfaces(); i++) {
+		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
+		for (int j = 0; j < 3; j++) {
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+			world_coords[j] = v;
+		}
+		Vec3f n = cross_multiply((world_coords[2] - world_coords[0]) , (world_coords[1] - world_coords[0]));
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0) {
+			std::vector<Vec2i> vertex_array{ screen_coords[0],screen_coords[1],screen_coords[2] };
+			triangle(vertex_array, image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+		}
+		
+	}
+	
+
+	image.flip_vertically();
+	image.write_tga_file("output.tga"); 
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
